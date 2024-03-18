3a5244b26c8eaa6838c6e7cca77cc53c72fdff88
Moahmmed Gomaa <mohammedgomaa2522@gmail.com>
2024-03-18 17:47:07 +0200

    edit swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,45 @@
 
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+
+// ********** Swiper Images **********
+const swiperJS = () => {
+    const swiper = new Swiper( '.swiper-container.two', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        effect: 'coverflow',
+        loop: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        coverflow: {
+            rotate: 0,
+            stretch: 100,
+            depth: 150,
+            modifier: 1.5,
+            slideShadows : false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        }         
+    } );
+};
+swiperJS();
+// -------------------------------------
+
 
 // ********** Scroll Sections **********
 const scrollSections = () => {
